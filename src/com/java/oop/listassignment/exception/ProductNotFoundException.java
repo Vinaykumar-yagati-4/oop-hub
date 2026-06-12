8075e1948f370be73d8c1b0ea52b962a3ef297d9
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-06-12 14:21:29 +0530

    feat : Exception added

--- src/com/java/oop/listassignment/exception/ProductNotFoundException.java
+++ src/com/java/oop/listassignment/exception/ProductNotFoundException.java
@@ -1,7 +1,8 @@
 package com.java.oop.listassignment.exception;
 
 public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
+    public ProductNotFoundException(String message)
+    {
         super(message);
     }
 }
