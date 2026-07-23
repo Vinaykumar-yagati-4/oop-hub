d5ba814fd0c48fd0ee1665195afe764bc453a0aa
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-07-23 17:45:32 +0530

    feat : secondlargestnumber in arrays

--- src/com/java/oop/arrays/FindSecondLargestElement.java
+++ src/com/java/oop/arrays/FindSecondLargestElement.java
@@ -3,16 +3,16 @@ package com.java.oop.arrays;
 public class FindSecondLargestElement {
     public static void main(String[] args) {
         int arr[] = {10,35,69,85,99};
-        int first = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
         for(int num: arr){
-            if(num > first){
-                second = first;
-                first = num;
-            }else if(num > second && num != first){
-                second = num;
+            if(num > largest){
+                secondLargest = largest;
+                largest = num;
+            }else if(num > secondLargest && num != largest){
+                secondLargest = num;
             }
         }
-        System.out.println("Second largest Number :" +second);
+        System.out.println("Second largest Number :" +secondLargest);
     }
 }
