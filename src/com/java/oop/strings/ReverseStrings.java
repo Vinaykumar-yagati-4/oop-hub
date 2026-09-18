ca2197a6d8d0d034c7fd90a544680f96063b05b1
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-09-18 19:32:10 +0530

    fix : ReverseStrings

--- src/com/java/oop/strings/ReverseStrings.java
+++ src/com/java/oop/strings/ReverseStrings.java
@@ -4,8 +4,8 @@ public class ReverseStrings {
     public static void main(String[] args) {
         String str = "java";
         String rev = "";
-        for (int i = str.length() - 1; i >= 0; i--){
-            rev += str.charAt(i);
+        for(int i = str.length()-1; i >= 0; i--){
+             rev = rev + str.charAt(i);
         }
 
         System.out.println(rev);
