9defb2874f358aa603c8999d111046d85799c657
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-09-19 18:45:30 +0530

    add : ReverseStrings using stringBuilder

--- src/com/java/oop/strings/ReverseStrings.java
+++ src/com/java/oop/strings/ReverseStrings.java
@@ -1,14 +1,22 @@
 package com.java.oop.strings;
 
 public class ReverseStrings {
-    public static void main(String[] args) {
-        String str = "java";
-        String rev = "";
-        for(int i = str.length()-1; i >= 0; i--){
-             rev = rev + str.charAt(i);
-        }
+//    public static void main(String[] args) {
+//        String str = "java";
+//        String rev = "";
+//        for(int i = str.length()-1; i >= 0; i--){
+//             rev = rev + str.charAt(i);
+//        }
+//
+//        System.out.println(rev);
+//    }
 
-        System.out.println(rev);
+    // using stringBuilder
+    static void main(String[] args) {
+       String str = "java";
+       String reverse = new StringBuilder(str)
+               .reverse()
+               .toString();
+        System.out.println(reverse);
     }
-
 }
