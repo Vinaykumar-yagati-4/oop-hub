11a9e7301561b9fd3999351bffaf2470b8d5a8eb
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-09-20 22:48:51 +0530

    fix : Anagrams

--- src/com/java/oop/strings/Anagrams.java
+++ src/com/java/oop/strings/Anagrams.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Anagrams {
     public static void main(String[] args) {
-        String s1 = "silent";
-        String s2 = "listen";
+        String s1 = "race";
+        String s2 = "care";
         char[] a1 = s1.toCharArray();
         char[] a2 = s2.toCharArray();
+
         Arrays.sort(a1);
         Arrays.sort(a2);
 
