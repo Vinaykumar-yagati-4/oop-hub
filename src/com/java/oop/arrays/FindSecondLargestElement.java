5a234c9f021f9183933a454aac32e0236a638fca
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-09-21 20:44:27 +0530

    feat : Add SecondLargestElement using arrays

--- src/com/java/oop/arrays/FindSecondLargestElement.java
+++ src/com/java/oop/arrays/FindSecondLargestElement.java
@@ -2,17 +2,19 @@ package com.java.oop.arrays;
 
 public class FindSecondLargestElement {
     public static void main(String[] args) {
-        int arr[] = {10,35,69,85,99};
+        int[] numbers = {10,30,50,70,90};
         int largest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
-        for(int num: arr){
-            if(num > largest){
+        for(int number : numbers){
+            if(number > largest){
                 secondLargest = largest;
-                largest = num;
-            }else if(num > secondLargest && num != largest){
-                secondLargest = num;
+                largest = number;
+            } else if (number > secondLargest && number != largest) {
+                secondLargest = number;
+
             }
         }
-        System.out.println("Second largest Number :" +secondLargest);
+        System.out.println("Largest :" +largest);
+        System.out.println("Second Largest :" +secondLargest);
     }
 }
