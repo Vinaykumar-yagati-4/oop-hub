4fc209c75b58185c605764cfbac0fff3717d02cd
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-09-23 20:27:56 +0530

    feat : add palindrome using strings

--- src/com/java/oop/strings/StringPalindrome.java
+++ src/com/java/oop/strings/StringPalindrome.java
@@ -3,12 +3,27 @@ package com.java.oop.strings;
 import java.util.Scanner;
 
 public class StringPalindrome {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        String input = sc.next();
-        String str = "";
-        String rev = new StringBuilder(str).reverse().toString();
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the data");
+//        String input = sc.next();
+//        String str = "";
+//        String rev = new StringBuilder(str)
+//        .reverse()
+//        .toString();
+//        if(str.equals(rev)){
+//            System.out.println("palindrome");
+//        }
+//        else
+//            System.out.println("not palindrome");
+//    }
+
+    static void main(String[] args) {
+        String str = "vinay";
+        String rev = "";
+        for(int i = str.length()-1; i >= 0; i--){
+            rev = rev + str.charAt(i);
+        }
         if(str.equals(rev)){
             System.out.println("palindrome");
         }
