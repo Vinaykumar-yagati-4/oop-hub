7348614d232dc3c9c9ae1d1309e289051ab09fdf
Vinaykumar-yagati-4 <vinaykumar774474@gmail.com>
2026-09-24 22:08:36 +0530

    fix : RemoveDuplicates using linked hashset

--- src/com/java/oop/arrays/RemoveDuplicates.java
+++ src/com/java/oop/arrays/RemoveDuplicates.java
@@ -25,7 +25,6 @@ public class RemoveDuplicates {
 
         int[] numbers = {20,10,40,30,40,50,10,30,60};
         Set<Integer> set = new LinkedHashSet<>();
-
         for (int number : numbers){
             set.add(number);
         }
